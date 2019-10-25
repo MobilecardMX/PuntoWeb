1cd9cd0ebb02fc89eb0947ee3b04aa1e14460117
Erick Quintanar <erick.quintanar@mobilecardcorp.com>
2019-10-24 21:38:21 -0500

    Se agrega pagina de respuesta

--- src/main/java/com/addcel/PuntoWeb/controller/FacPagoController.java
+++ src/main/java/com/addcel/PuntoWeb/controller/FacPagoController.java
@@ -11,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Log4j
@@ -47,6 +44,11 @@ public class FacPagoController {
             return new ModelAndView("error");
         }
         return new ModelAndView("facPago");
+    }
+
+    @PostMapping("3d/response")
+    public ModelAndView response3d(){
+        return new ModelAndView("3dResponse");
     }
 
     /**
